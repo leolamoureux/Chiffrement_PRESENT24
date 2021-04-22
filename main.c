6f55f987bec05225cff715ed85149032ff2249b0
leolamoureux <leo.salagnac@gmail.com>
2021-04-22 17:59:17 +0200

    changement string main

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int  main (int argc, char *argv[]) {
 
     printf("message : %s, clé : %s\n",argv[1],argv[2]);
 
-    char message[24]="000001111122222333334444";
+    char *message="000001111122222333334444";
     char *message2 = permutation(message);
     printf("message2= %s\n",message2);
 
