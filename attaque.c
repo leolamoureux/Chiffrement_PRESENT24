27389fa92b9d13bc523c08a5b8029fb5fa9e0944
leolamoureux <leo.salagnac@gmail.com>
2021-04-29 16:34:37 +0200

    2.2

--- attaque.c
+++ attaque.c
@@ -1,10 +1,14 @@
 #include "attaque.h"
-
-ListeM remplir_liste_M(unsigned int message_clair[24]){
-    struct ListeM LM;
-    return LM;
-}
-ListeC remplir_liste_C(unsigned int message_chiffre[24]){
-    struct ListeC LC;
-    return LC;
+#include "PRESENT24.h"
+/*2^24=16777216*/
+void comparaison_listes(unsigned int message_clair[24], unsigned int message_chiffre[24]){
+    for(int i=0;i<5;i++){
+        /*cadencement pour chaque clé i */
+        /*chiffrement message clair avec le cadencement*/
+        for(int j=0;j<5;j++){
+            /*cadencement pour la clé j*/
+            /*chiffrement(message_chiffré,cadencement)*/
+            /*comparaison messageclair(i) avec tous les message chiffrés*/
+        }
+    }
 }
--- attaque.h
+++ attaque.h
@@ -3,22 +3,6 @@
 #include  <string.h>
 #include  <math.h>
 
-#define TAILLE_LISTE 100
-
-/*2^24=16777216 clés possibles pour une taille de 24bits*/
-
-/*liste M*/
-typedef struct ListeM{
-    unsigned int chiffre[TAILLE_LISTE][24];
-    unsigned int cle[TAILLE_LISTE][24];
-}ListeM;
-
-/*Liste C*/
-typedef struct ListeC{
-    unsigned int clair[TAILLE_LISTE][24];
-    unsigned int cle[TAILLE_LISTE][24];
-}ListeC;
 
 /*fonctions*/
-ListeM remplir_liste_M(unsigned int message_clair[24]);
-ListeC remplir_liste_C(unsigned int message_chiffre[24]);
+void comparaison_listes(unsigned int message_clair[24], unsigned int message_chiffre[24]);
