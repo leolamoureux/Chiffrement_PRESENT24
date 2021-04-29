95b214777ea772c3be41500c78cf9eb0e0294d42
leolamoureux <leo.salagnac@gmail.com>
2021-04-29 17:17:56 +0200

    2.3

--- attaque.c
+++ attaque.c
@@ -2,12 +2,38 @@
 #include "PRESENT24.h"
 /*2^24=16777216*/
 void comparaison_listes(unsigned int message_clair[24], unsigned int message_chiffre[24]){
+    unsigned int tab_i[24],tab_j[24],iter_i,iter_j;
     for(int i=0;i<5;i++){
-        /*cadencement pour chaque clé i */
-        /*chiffrement message clair avec le cadencement*/
+
+        /*conversion de i en binaire*/
+        iter_i=i;
+        for(int x=0;iter_i>0;x++){  
+            tab_i[23-x]=iter_i%2;  
+            iter_i=iter_i/2; 
+        }
+        /*on met tous les autres bits non utilisés à 0*/
+        for(int a=0;a<24;a++){
+            if(tab_i[a]!=1)tab_i[a]=0;
+        }
+        /*on fait le cadencement pour chaque cle i = chaque valeur de tab de 0 à 2^24*/
+        CLES K_i=cadencement(tab_i);
+        chiffrement(message_clair,K_i);
         for(int j=0;j<5;j++){
-            /*cadencement pour la clé j*/
-            /*chiffrement(message_chiffré,cadencement)*/
+
+            /*conversion de j en binaire*/
+            iter_j=j;
+            for(int x=0;iter_j>0;x++){  
+                tab_j[23-x]=iter_j%2;  
+                iter_j=iter_j/2; 
+            }
+            /*on met tous les autres bits non utilisés à 0*/
+            for(int a=0;a<24;a++){
+                if(tab_j[a]!=1)tab_j[a]=0;
+            }
+            /*on fait le cadencement pour chaque cle j*/
+            CLES K_j=cadencement(tab_j);
+            chiffrement(message_clair,K_j);
+            
             /*comparaison messageclair(i) avec tous les message chiffrés*/
         }
     }
